5986a9a5682707cf2dc84a61ee596a2ab32ae3f1
lichen404 <f360family@gmail.com>
2023-08-07 16:47:01 +0800

    使用 Rspec 测试

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes,only: [:create]
+      resource :session, only: [:create,:destory]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 end
---
+++ db/migrate/20230806125603_create_validation_codes.rb
@@ -0,0 +1,11 @@
+class CreateValidationCodes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :validation_codes do |t|
+      t.string :email
+      t.integer :kind, default: 1,null: false
+      t.string :used_at
+      t.string  :code, limit: 20
+      t.timestamps
+    end
+  end
+end
